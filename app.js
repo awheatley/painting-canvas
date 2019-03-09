a385913f0aa2c819ca44057fa220c319df4ba5b0
Adam Wheatley <adam.wheatley@caddis.co>
2019-03-09 09:50:57 -0600

    Update draw function and mousedown listener

--- app.js
+++ app.js
@@ -16,10 +16,32 @@ let lastY = 0;
 
 const draw = (e) => {
     if (!isDrawing) return;  // Function doesn't run if not mousedown
+
+    // Start a path
+    ctx.beginPath
+
+    // Start the line here
+    ctx.moveTo(lastX, lastY);
+
+    // End the line here
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    // Update lastX and lastY for new starting points
+    lastX = e.offsetX;
+    lastY = e.offsetY;
 };
 
 c.addEventListener('mousemove', draw);
-c.addEventListener('mousedown', () => isDrawing = true);
+
+c.addEventListener('mousedown', (e) => {
+    // Update lastX and lastY with current mouse position
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    
+    isDrawing = true;
+});
+
 c.addEventListener('mouseup', () => isDrawing = false);
 
 // If mouse goes outside window
